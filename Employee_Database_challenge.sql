2407f3b2a37973af4665c8edc21b18cd200ad4d4
Asad Khan <asad_ca4u@hotmail.com>
2020-08-13 13:24:27 -0400

    Completed Deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -27,3 +27,21 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY (ut.title)
 ORDER BY COUNT(ut.title) DESC;
+
+--Deliverable 2: mentorship-eligibility table
+--holds the current employees who were born between January 1, 1965 and December 31, 1965
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
